76060a71211fe5805945ebd583184cfca1db74bc
sesa736123 <mateusz.szkop@non.se.com>
2025-03-16 11:05:33 +0100

    feat: add commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,24 @@ import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
+/** typescript-eslint enables ESLint to run on TypeScript code. Also adds new rules specific to TS. */
 export default ts.config(
+	/** Ignores files from .gitignore from being linted. May not be needed when lint-staged used. */
 	includeIgnoreFile(gitignorePath),
+	/** Recommended rules for JS */
 	js.configs.recommended,
+	/** Recommended rules for TS */
 	...ts.configs.recommended,
+	/** Recommended rules for Svelte */
 	...svelte.configs.recommended,
+	/** Disables eslint rules that are unnecessary or might conflict with Prettier. */
 	prettier,
+	/** Disables Svelte eslint rules that are unnecessary or might conflict with Prettier. */
 	...svelte.configs.prettier,
+	/** Lists global (like window, document, process, __dirname) for Eslint to know they don't require import */
 	{
 		languageOptions: {
 			globals: {
@@ -23,10 +32,10 @@ export default ts.config(
 			}
 		}
 	},
+	/** Eslint config for Svelte files */
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		ignores: ['eslint.config.js', 'svelte.config.js'],
-
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
